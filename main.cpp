de100b1344b9580652ba16661d025d062e509ad7
pezetaprog <andrescps101@gmail.com>
2026-03-06 17:20:40 -0500

    archivos actualizados

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@
 
 using namespace std;
 
-class Consola
+class Menu
 {
 public:
     void mostarMenu()
     {
-        cout << "\n-MENU-" << endl;
+        cout << "MENU" << endl;
         cout << "1: Modulo" << endl;
         cout << "2: Division" << endl;
         cout << "3: Historial" << endl;
@@ -20,7 +20,7 @@ public:
     }
 };
 
-class Recibirdata
+class RecibirDato
 {
 public:
     void ingresonumeros(int &num1, int &num2)
@@ -48,6 +48,8 @@ class Modulo
 public:
     int mod(int num1, int num2)
     {
+        if (num2 == 0)
+            return 0;
         return num1 % num2;
     }
 };
@@ -57,16 +59,10 @@ class Historial
 public:
     vector<string> operaciones;
 
-public:
     void agregar(string operacion, int num1, int num2, int resultado)
     {
         string linea = operacion + ": " + to_string(num1) + " , " + to_string(num2) + " = " + to_string(resultado);
         operaciones.push_back(linea);
-    }
-
-    vector<string> obtenerOperaciones()
-    {
-        return operaciones;
     }
 
     void mostrar()
@@ -115,32 +111,16 @@ public:
     }
 };
 
-class Imprimir
-{
-private:
-    PantallaConsola consola;
-    PantallaArchivo archivo;
-
-public:
-    void mostrarEnConsola(int resultado)
-    {
-        consola.mostrarResultado(resultado);
-    }
-
-    void guardarEnArchivo(int resultado, string nombreArchivo)
-    {
-        archivo.guardarResultado(resultado, nombreArchivo);
-    }
-};
 
 int main()
 {
-    Consola menu;
-    Recibirdata recibir;
+    Menu menu;
+    RecibirDato recibir;
     Division division;
     Modulo modulo;
     Historial historial;
-    Imprimir imprimir;
+    PantallaConsola cons;
+    PantallaArchivo arch;
 
     int opcion;
     int num1, num2, resultado;
@@ -156,16 +136,16 @@ int main()
             recibir.ingresonumeros(num1, num2);
             resultado = modulo.mod(num1, num2);
             historial.agregar("Modulo", num1, num2, resultado);
-            imprimir.mostrarEnConsola(resultado);
-            imprimir.guardarEnArchivo(resultado, "resultados.txt");
+            cons.mostrarResultado(resultado);
+            arch.guardarResultado(resultado, "resultados.txt");
             break;
 
         case 2:
             recibir.ingresonumeros(num1, num2);
             resultado = division.div(num1, num2);
             historial.agregar("Division", num1, num2, resultado);
-            imprimir.mostrarEnConsola(resultado);
-            imprimir.guardarEnArchivo(resultado, "resultados.txt");
+            cons.mostrarResultado(resultado);
+            arch.guardarResultado(resultado, "resultados.txt");
             break;
 
         case 3:
@@ -180,7 +160,7 @@ int main()
             cout << "Opcion invalida." << endl;
         }
 
-    } while (opcion != 0);
+    } while (opcion != 4);
 
     return 0;
 }
