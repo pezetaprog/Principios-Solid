c0a5223439f547570036414cbbaa6e917bd61479
pezetaprog <andrescps101@gmail.com>
2026-03-06 12:13:02 -0500

    se agrega el proyecto

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+class Consola{};
+class Recibirdata{};
+class Historial{};
+class Imprimir{};
+class Division{};
+class Suma{};
+class Resta{};
+class Multiplicacion{};
+
+
+
+int main(){
+    cout << ("hola");
+    
+}
